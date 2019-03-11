6265ea87e4e3d27b32c815b7b48721e51a1dbf32
Bosco <bleung9@gmail.com>
2019-03-11 03:06:27 +0000

    https example step 5

--- https_example.js
+++ https_example.js
@@ -1,3 +1,35 @@
 var https = require("https");
 
-console.log("I did it!");
+var options = {
+  host: "www.google.ca",
+  path: "/"
+}
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log("in response handler callback!");
+  console.log("Response: ", response);
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("I did it!");
